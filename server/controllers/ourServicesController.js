6b9bbe9f75cc60cba1b8c0fbfb2a3a58415c1324
Victor Silva <victormarques.ia@gmail.com>
2020-07-01 11:28:03 -0300

    feat(our-services): Create getOurServices function in ourServicesController

--- server/controllers/ourServicesController.js
+++ server/controllers/ourServicesController.js
@@ -4,5 +4,15 @@ const ourServices = keystone.list('NossosServiços');
 
 module.exports = {
 
-  
+  getOurServices(req, res) {
+    ourServices.model.find((err, items) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('DB Error');
+      } 
+
+     res.status(200).json(items)
+    });
+  }
+
 }
