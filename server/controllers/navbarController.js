65d7189737457c99360cac666de073d97cad6b8f
Victor Silva <victormarques.ia@gmail.com>
2020-06-30 21:27:31 -0300

    feat(navbar): Create function getNavbar in navbarController

--- server/controllers/navbarController.js
+++ server/controllers/navbarController.js
@@ -3,5 +3,14 @@ const keystone = require('keystone');
 const navbar = keystone.list('navbar');
 
 module.exports = {
-  
+  getNavbar(req, res) {
+    navbar.model.find((err, items) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('DB Error');
+      } 
+
+     res.status(200).json(items)
+    });
+  }
 }
