5a83edb1dde2f5ebacacfc11e19f1a8a854f5452
Victor Silva <victormarques.ia@gmail.com>
2020-06-30 21:19:16 -0300

    fix(unused-files): Remove route to model 'Example'

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,6 @@ const path = require('path');
 const keystone = require('keystone');
 const cors = require('cors');
 
-const Example = keystone.list('Examples');
-
 module.exports = (app) => {
   app.use(cors());
 
@@ -11,10 +9,4 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
 
-  app.get('/api/examples', (req, res) => {
-    Example.model.find((err, items) => {
-      if (err) return res.apiError('database error', err);
-      res.send(items);
-    });
-  });
 };
