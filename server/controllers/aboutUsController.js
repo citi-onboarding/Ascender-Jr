6e585b40610c6dbb473a0e617910d2392f3a695a
Victor Silva <victormarques.ia@gmail.com>
2020-07-01 12:21:48 -0300

    feat(about-us): Create function getAboutUs in aboutUsController

--- server/controllers/aboutUsController.js
+++ server/controllers/aboutUsController.js
@@ -4,5 +4,15 @@ const aboutUs = keystone.list('SobreNós');
 
 module.exports = {
 
+  getAboutUs(req, res) {
+    aboutUs.model.find((err, items) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('DB Error');
+      } 
+
+     res.status(200).json(items)
+    });
+  }
   
 }
