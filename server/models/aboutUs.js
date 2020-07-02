883efc0ab149982eb46338b62b035fdbda78c08a
Victor Silva <victormarques.ia@gmail.com>
2020-07-02 11:34:03 -0300

    upgrade(about-us): Changes the text fields of the model 'aboutUs' to Textarea

--- server/models/aboutUs.js
+++ server/models/aboutUs.js
@@ -2,6 +2,7 @@ const keystone = require('keystone');
 
 const { Types } = keystone.Field;
 
+
 // First we gonna create a aboutUs list
 const aboutUs = new keystone.List('SobreNós', {
   map: { name: 'key' },
@@ -11,18 +12,18 @@ const aboutUs = new keystone.List('SobreNós', {
 aboutUs.add({
   key: { type: Types.Text, default: 'Sobre Nós', initial: false },
   vision: {
-    text: { type: Types.Text, required: true, initial: true, label: 'Visão' },
+    text: { type: Types.Textarea, required: true, initial: true, label: 'Visão' },
     image_url: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem da visão'},
   },
   mission: {
-    text: { type: Types.Text, required: true, initial: true, label: 'Missão' },
+    text: { type: Types.Textarea, required: true, initial: true, label: 'Missão' },
     imageUrl: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem da missão'},
   },
   values: {
     valueArray: { type: Types.TextArray, required: true, initial: true, label: 'Valores' },
     imageUrl: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem dos valores'},
   },
-  companyHistory: { type: Types.Text, required: true, initial: true, label: 'História da empresa'}
+  companyHistory: { type: Types.Textarea, required: true, initial: true, label: 'História da empresa'}
 });
 
 aboutUs.register();
