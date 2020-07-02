548eb1ef94bfdb9f2b724a756e6399e2ebc8bb20
Victor Silva <victormarques.ia@gmail.com>
2020-07-02 16:01:18 -0300

    upgrade(our-services): Refactored some fields in the 'ourServices' model

--- server/models/ourServices.js
+++ server/models/ourServices.js
@@ -4,24 +4,25 @@ const { Types } = keystone.Field;
 
 // First we gonna create a ourServices list
 const ourServices = new keystone.List('NossosServiços', {
-  map: { name: 'service.text' },
+  map: { name: 'serviceName' },
 });
 
 // Then we gonna add the fields
 ourServices.add({
-  service: {
-    text: { type: Types.Text, required: true, initial: true, label: 'Serviço' },
-    imageUrl: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem do serviço'},
+    serviceName: { type: Types.Text, required: true, initial: true, label: 'Serviço' },
+    serviceDescription: { type: Types.Textarea, required: true, initial: true, label: 'Descrição'},
+    serviceImage: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem do serviço'},
 
     beforeService: {
       text: { type: Types.Text, required: true, initial: true, label: 'Legenda antes do serviço' },
+      description: { type: Types.Textarea, required: true, initial: true, label: 'Descrição'},
       imageUrl: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem antes'},
     },
     
     afterService: {
       text: { type: Types.Text, required: true, initial: true, label: 'Legenda depois do serviço' },
+      description: { type: Types.Textarea, required: true, initial: true, label: 'Descrição'},
       imageUrl: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem depois'},
-    }
   },
 })
 
