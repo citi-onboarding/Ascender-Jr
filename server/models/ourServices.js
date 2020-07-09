3b161278dcb61cbeeabe6f0cf3509459edc8511e
Victor Silva <victormarques.ia@gmail.com>
2020-07-09 20:37:29 -0300

    upgrade(our-services): Refactor model 'ourServices'

--- server/models/ourServices.js
+++ server/models/ourServices.js
@@ -3,26 +3,25 @@ const keystone = require("keystone");
 const { Types } = keystone.Field;
 
 // First we gonna create a ourServices list
-const ourServices = new keystone.List('NossosServiços', {
+const ourServices = new keystone.List('OurServices', {
   map: { name: 'serviceName' },
+  label: 'Nossos serviços'
 });
 
 // Then we gonna add the fields
 ourServices.add({
-    serviceName: { type: Types.Text, required: true, initial: true, label: 'Serviço' },
-    serviceDescription: { type: Types.Textarea, required: true, initial: true, label: 'Descrição'},
+    serviceName: { type: Types.Text, required: true, initial: true, label: 'Serviço', note: 'Digite o nome do serviço' },
+    serviceDescription: { type: Types.Textarea, required: true, initial: true, label: 'Descrição', note: 'Fale sobre este serviço'},
     serviceImage: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem do serviço'},
 
     beforeService: {
       text: { type: Types.Text, required: true, initial: true, label: 'Legenda antes do serviço' },
-      description: { type: Types.Textarea, required: true, initial: true, label: 'Descrição'},
-      imageUrl: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem antes'},
+      image: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem antes'},
     },
     
     afterService: {
       text: { type: Types.Text, required: true, initial: true, label: 'Legenda depois do serviço' },
-      description: { type: Types.Textarea, required: true, initial: true, label: 'Descrição'},
-      imageUrl: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem depois'},
+      image: { type: Types.CloudinaryImage, required: true, initial: true, label: 'Imagem depois'},
   },
 })
 
