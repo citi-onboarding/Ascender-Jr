0f6c31b8b13a9fa254d3990211ddf84e94b57b7a
Victor Silva <victormarques.ia@gmail.com>
2020-07-13 00:50:57 -0300

    feat(our-services-component): Create OurServices component

---
+++ client/src/components/OurServices/OurServices.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
+
+import api from '../../services/api';
+
+const OurServices = () => {
+
+
+  const [ourServices, setOurServices] = useState([]);
+
+  useEffect(() => {
+
+    api.get('our-services').then(response => {
+      const dataOurServices = response.data;
+
+      setOurServices(dataOurServices);
+    });
+
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <section id="section-our-services">
+      <div className="container-carousel">
+          <h1>Serviços</h1>
+        <Slider {...settings}>
+        
+          {ourServices.map(ourService => (
+            
+            <div key={ourService.serviceName} className="card">
+              <div className="primary-card">
+                <div className="content">
+                  <img className="primary-card-image" src={ourService.serviceImage.url} alt=""/>
+                  <div className="description">
+                    <h3>{ourService.serviceName}</h3>
+                    <p>{ourService.serviceDescription}</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="secondary-card">
+
+                <div className="box-before">
+                  <div className="content">
+                    <img className="before-image" src={ourService.beforeService.image.secure_url} alt=""/>
+                    <div className="description">
+                      <h3>{ourService.beforeService.text}</h3>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="box-after">
+                  <div className="content">
+                    <img className="after-image" src={ourService.afterService.image.secure_url} alt=""/>
+                    <div className="description">
+                      <h3>{ourService.afterService.text}</h3>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+          
+          </div>
+          ))}
+        </Slider>
+
+      </div>
+
+    </section>  
+  );
+}
+
+export default OurServices;
