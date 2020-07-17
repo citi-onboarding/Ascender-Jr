37f378fb3da62541cbf3cf1aef35940c1a4dc950
Maria Beatriz Bezerra <38388224+mmariabeatriz@users.noreply.github.com>
2020-07-17 18:49:19 -0300

    Add files via upload

---
+++ server/controllers/mailController.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+const mailServer = require('./mailServer');
+
+const sendMail = async (req, res, next) => {
+  try {
+    const { name, email, subject, message } = req.body;
+
+    const body = `
+      Nome: ${name}
+      Email: ${email}
+      Mensagem: ${message}`;
+
+    await mailServer({
+      destinationUser: process.env.CLIENT_EMAIL,
+      subjectText: subject,
+      textOption: body,
+    });
+
+    res.status(200).send('');
+  } catch (error) {
+    res.status(500).send('Ops, something is wrong...');
+    console.log(error);
+  }
+};
+
+module.exports = sendMail;
