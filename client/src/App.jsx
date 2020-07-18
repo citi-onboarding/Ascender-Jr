84d0f3bd0d00f81856a6b9d3cf8eb5315561a0c5
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-07-18 11:07:11 -0300

    feat(contact): Fixed linked social media.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './components/Banner';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
     <section className="app">
       <Banner />
+      <Contact />
     </section>
   );
 }
