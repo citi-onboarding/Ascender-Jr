832985bd60ed86db9636037b793fd7eb7cf1a134
Maria Beatriz Bezerra <38388224+mmariabeatriz@users.noreply.github.com>
2020-07-18 05:05:25 -0300

    feat/Extra
    **What I did:**
    -"Extra" pop-up created;

---
+++ client/src/components/EXTRA/extra.jsx
@@ -0,0 +1,20 @@
+
+import React, {Component} from 'react';
+import './extra.css';
+
+import extra from './extra.png';
+
+class Extra extends React.Component {
+    render() {
+        return (
+            <div class="fundo-extra">
+            <div class="box-extra">
+                <button class="close-extra">X</button>
+                <p>Ops, ainda trabalhando nessa seção</p>
+            </div>
+        </div>
+        );  
+    }
+
+}
+export default Extra;
