c3591e4675c350fc5f25036330aa364aede65910
Victor Marques <victormarques.ia@gmail.com>
2020-07-23 17:48:52 -0300

    fix(upgrade/navbar): Fix file path in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Banner from "./components/Banner";
 import AboutUs from "./components/AboutUs/AboutUs";
 import OurServices from "./components/OurServices/OurServices";
