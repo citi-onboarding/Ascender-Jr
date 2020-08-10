97ad87f3905b80119082f4bce97d592b1a9e3d13
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-08-10 12:03:31 -0300

    develop(contact): Added blank open to links and whatsapp href.

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,118 +1,170 @@
-import React, { useReducer } from 'react';
+import React, { useReducer } from "react";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
-import './Contact.css';
+import "./Contact.css";
 
-import instagram from '../../assets/instagram.png';
-import whatsapp from '../../assets/whatsapp.png';
-import gmail from '../../assets/gmail.png';
-import linkedin from '../../assets/linkedin.png';
-import facebook from '../../assets/facebook.png';
-import userform from '../../assets/user-form.png';
-import envelope from '../../assets/envelope-form.png';
-import card from '../../assets/card-form.png';
-import note from '../../assets/note-form.png';
-import code from '../../assets/code.svg';
-import coracao from '../../assets/coracao.svg';
-import citi from '../../assets/citi-logo.png';
-
+import instagram from "../../assets/instagram.png";
+import whatsapp from "../../assets/whatsapp.png";
+import gmail from "../../assets/gmail.png";
+import linkedin from "../../assets/linkedin.png";
+import facebook from "../../assets/facebook.png";
+import userform from "../../assets/user-form.png";
+import envelope from "../../assets/envelope-form.png";
+import card from "../../assets/card-form.png";
+import note from "../../assets/note-form.png";
+import code from "../../assets/code.svg";
+import coracao from "../../assets/coracao.svg";
+import citi from "../../assets/citi-logo.png";
 
 const initialState = {
-    name: '',
-    email: '',
-    subject: '',
-    message: ''
+  name: "",
+  email: "",
+  subject: "",
+  message: "",
 };
 
 const reducer = (state, action) => {
+  if (action.type === "reset") {
+    return initialState;
+  }
 
-    if (action.type === "reset") {
-        return initialState;
-    }
-
-    const result = { ...state };
-    result[action.field] = action.value;
-    return result;
+  const result = { ...state };
+  result[action.field] = action.value;
+  return result;
 };
 
 const Contact = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { name, email, subject, message } = state;
 
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const { name, email, subject, message } = state;
+  const handleChange = (e) => {
+    dispatch({ field: e.target.name, value: e.target.value });
+  };
 
-    const handleChange = (e) => {
-        dispatch({ field: e.target.name, value: e.target.value });
-    }
+  const sendContact = (e) => {
+    e.preventDefault();
 
+    api.post("send-email", state).then((response) => {
+      dispatch({ type: "reset" });
+    });
+  };
 
-    const sendContact = (e) => {
-        e.preventDefault();
-
-        api.post('send-email', state).then(response => {
-        
-            dispatch({ type: "reset" });
-        })
-        
-    }
-
-    return (
-        <div id="secao-contato">
-            <div className="blur-degrade">
-                <div className="infos-contato">
-                    <div className="container-whatsapp">
-                        <img className="svg-whatsapp" src= { whatsapp } alt=""/>
-                        <p>(31) 9 8505-6323</p>
-                    </div>
-                    <div className="container-email">
-                        <img className="svg-email" src= { gmail } alt=""/>
-                        <p>ascenderjr@gmail.com</p>
-                    </div>
-                    <div className="container-social-media">
-                        <a href="https://www.instagram.com/ascender.jr/" id="instagram"><img className="instagram" src= { instagram } alt=""/></a>
-                        <a href="https://www.linkedin.com/company/ascender-jr-/" id="linkedin"><img className="linkedin" src= { linkedin } alt=""/></a>
-                        <a href="https://www.facebook.com/Ascender.Junior"><img className="facebook" src= { facebook } alt=""/></a>
-                    </div>
-                    <p className="ascender-info">Ascender Jr. - Soluções em Energia | © 2020 Universidade Federal de Ouro Preto CNPJ: 20.413.778/0001-30</p>
-                </div>
-                <div className="form-contato">
-                    <h2>Contato</h2>
-                    <form onSubmit={sendContact}>
-                        <div className="alinhar-svg-input">
-                            <img className="svg-input" src= { userform } alt=""/>
-                            <input type="text" placeholder="Nome" name="name" value={name} onChange={handleChange}/>
-                        </div>
-                        <div className="alinhar-svg-input">
-                            <img className="svg-input" src= { envelope } alt=""/>
-                            <input type="text" placeholder="E-mail" name="email" value={email} onChange={handleChange}/>
-                        </div>
-                        <div className="alinhar-svg-input">
-                            <img className="svg-input" src= { card } alt=""/>
-                            <input type="text" placeholder="Assunto" name="subject" value={subject} onChange={handleChange}/>
-                        </div>
-                        <div className="alinhar-svg-input">
-                            <img className="form-msg-img svg-input" src= { note } alt=""/>
-                            <textarea id="" cols="30" rows="10" placeholder="Mensagem" name="message" value={message} onChange={handleChange}/>
-                        </div>  
-                        <button type="submit">ENVIAR</button>
-                    </form>
-                </div>
-            </div>
-            <footer>
-                <p>
-                    Made with
-                    <img src= { code } className="ch-footer" alt=""/>
-                    and
-                    <img src= { coracao } className="ch-footer" alt=""/>
-                    by
-                    <a href="https://www.citi.org.br/">
-                        <img id="citi-logo" src= { citi } alt=""/>
-                    </a>
-                </p>
-            </footer>
+  return (
+    <div id="secao-contato">
+      <div className="blur-degrade">
+        <div className="infos-contato">
+          <div className="container-whatsapp">
+            <a target="_blank" rel="noopener noreferrer" href="https://wa.me/31985056323">
+              <img className="svg-whatsapp" src={whatsapp} alt="" />
+            </a>
+            <p>(31) 9 8505-6323</p>
+          </div>
+          <div className="container-email">
+            <img className="svg-email" src={gmail} alt="" />
+            <p>ascenderjr@gmail.com</p>
+          </div>
+          <div className="container-social-media">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/ascender.jr/"
+              id="instagram"
+            >
+              <img className="instagram" src={instagram} alt="" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/company/ascender-jr-/"
+              id="linkedin"
+            >
+              <img className="linkedin" src={linkedin} alt="" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/Ascender.Junior"
+            >
+              <img className="facebook" src={facebook} alt="" />
+            </a>
+          </div>
+          <p className="ascender-info">
+            Ascender Jr. - Soluções em Energia | © 2020 Universidade Federal de
+            Ouro Preto CNPJ: 20.413.778/0001-30
+            <br />
+            <br />
+            Rua Trinta e Seis, n 115 - Bloco B, Sala 301 Loanda,João Monlevade -
+            MG , 35931-008
+          </p>
         </div>
-    );
-}
-
+        <div className="form-contato">
+          <h2>Contato</h2>
+          <form onSubmit={sendContact}>
+            <div className="alinhar-svg-input">
+              <img className="svg-input" src={userform} alt="" />
+              <input
+                type="text"
+                placeholder="Nome"
+                name="name"
+                value={name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="alinhar-svg-input">
+              <img className="svg-input" src={envelope} alt="" />
+              <input
+                type="text"
+                placeholder="E-mail"
+                name="email"
+                value={email}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="alinhar-svg-input">
+              <img className="svg-input" src={card} alt="" />
+              <input
+                type="text"
+                placeholder="Assunto"
+                name="subject"
+                value={subject}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="alinhar-svg-input">
+              <img className="form-msg-img svg-input" src={note} alt="" />
+              <textarea
+                id=""
+                cols="30"
+                rows="10"
+                placeholder="Mensagem"
+                name="message"
+                value={message}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit">ENVIAR</button>
+          </form>
+        </div>
+      </div>
+      <footer>
+        <p>
+          Made with
+          <img src={code} className="ch-footer" alt="" />
+          and
+          <img src={coracao} className="ch-footer" alt="" />
+          by
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.citi.org.br/"
+          >
+            <img id="citi-logo" src={citi} alt="" />
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+};
 
 export default Contact;
